4caa6a1a0ea1449e6c6cc3cc61baab7b90dc25aa
Cyril Russo <boite.pour.spam@gmail.com>
2016-08-23 12:17:26 +0200

    Add support for progress bar while synchronizing the library

--- resources/assets/js/components/main-wrapper/main-content/settings.vue
+++ resources/assets/js/components/main-wrapper/main-content/settings.vue
@@ -15,12 +15,23 @@
 
         <input type="text" v-model="state.settings.media_path" id="inputSettingsPath">
       </div>
+    
+      <div class="form-row">
+        <label for="inputSettingsPublic">Media Path Sharing</label>
+        <p class="help">
+          If checked, the songs found in the path set above are visible to all users.
+          Otherwise, only you will be able to see these songs.
+        </p>
+
+        <input type="checkbox" v-model="state.settings.media_path_public" id="inputSettingsPublic">
+      </div>
 
       <div class="form-row">
         <button type="submit">Save settings</button>
         <button type="button" @click="scanLibrary">Scan library</button>
       </div>
     </form>
+    <sync-update ref="syncUpdate"></sync-update>
   </section>
 </template>
 
@@ -29,9 +40,11 @@ import { settingStore, sharedStore } from '@/stores'
 import { parseValidationError, forceReloadWindow, showOverlay, hideOverlay, alerts } from '@/utils'
 import { http } from '@/services';
 import router from '@/router'
+import syncUpdate from '@/components/modals/sync-update.vue';
 
 export default {
   name: 'main-wrapper--main-content--settings',
+  components: { syncUpdate },
 
   data () {
     return {
@@ -87,30 +100,13 @@ export default {
     },
 
     /**
-     * Scan the media path
+     * Scan the given path
      */
     scanLibrary() {
-      showOverlay()
-
-      new Promise((resolve, reject) => {
-          http.post('syncLibrary', null, data => resolve(data), r => reject(r))
-      }).then(r => {
-          // Make sure we're back to home first.
-          router.go('home')
-          forceReloadWindow()
-      }).catch(r => {
-          let msg = 'Unknown error.'
-
-          if (r.status === 422) {
-            msg = parseValidationError(r.responseJSON)[0]
-          }
-
-          hideOverlay()
-          alerts.error(msg)
-      })
+      this.$refs.syncUpdate.startSync()
     }
   }
-}
+};
 </script>
 
 <style lang="scss">
